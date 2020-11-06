8f4b52ed6688f9901dc31777dc5df06dba34df91
DeveloperSerhii <example@gmail.com>
2020-11-06 13:50:28 +0200

    Add second Function

--- JS/script_training.js
+++ JS/script_training.js
@@ -26,3 +26,27 @@ function start () {
 
 start();
 
+const personalMovieDB = {
+    count: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов?", ''),
+              b = prompt("На сколько оцените его?", '');
+
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log("Done");
+            } else {
+                console.log("Error");
+                i--;
+            }
+    }
+}
+
+rememberMyFilms();
+
